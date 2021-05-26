8306fbce8c7d09ccccd3f9a18669705428a882a3
OldTrace <liyedadi@gmail.com>
2021-05-26 11:24:14 +0800

    first commit

---
+++ src/main/java/git/Try.java
@@ -0,0 +1,13 @@
+package git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @Author OLdTrace
+ * @Date 2021/5/26 11:13
+ */
+class Try {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
